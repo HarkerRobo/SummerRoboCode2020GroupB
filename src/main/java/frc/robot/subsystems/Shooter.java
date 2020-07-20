7fdbf8e74275c9106ef679ed82cebe9ce20154b2
Angela Jia <42385351+22angiej@users.noreply.github.com>
2020-07-19 18:12:14 -0700

    Fix Shooter to accomodate  HarkerRoboLib changes
    Co-authored-by: 23adap <23adap@students.harker.org>
    Co-authored-by: SHAD0WV1RUS <jainkunal2005@gmail.com>

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,15 +1,19 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import frc.robot.RobotMap;
 import harkerrobolib.subsystems.HSFlywheel;
+import harkerrobolib.util.Gains;
 import harkerrobolib.wrappers.HSFalcon;
 import harkerrobolib.wrappers.HSMotorController;
 
 /**
  * Represents a shooter on the 2020 robot.
+ * 
+ * @author Kunal Jain
  */
 public class Shooter extends HSFlywheel<HSMotorController> {
 
@@ -20,20 +24,23 @@ public class Shooter extends HSFlywheel<HSMotorController> {
    public static final int TICKS_PER_REVOLUTION = 2048;
 
    public static final boolean SENSOR_PHASE = false;
-   public static final TalonFXInvertType MASTER_INVERT = TalonFXInvertType.Clockwise;
-   public static final TalonFXInvertType FOLLOWER_INVERT = TalonFXInvertType.CounterClockwise;
+   public static final boolean MASTER_INVERT = true;
+   public static final boolean FOLLOWER_INVERT = false;
    public static final int VELOCITY_SLOT = 0;
-   public static double[] velocityPIDConstants = {0.064, 0.05, 0.001, 0.9, 150}; // {kF, kP, kI, kD, iZone}
+   public static Gains velocityPIDConstants = new Gains(0.064, 0.05, 0.001, 0.9, 150); // kF, kP, kI, kD, iZone
    public static final double VOLTAGE_COMP = 9.5;
    public static final double PEAK_CURRENT = 60;
    public static final double SUSTAINED_CURRENT = 50;
+   public static final StatorCurrentLimitConfiguration CURRENT_LIMIT = new StatorCurrentLimitConfiguration(true,0.2,10,100);
    public static final double PEAK_TIME = 0.2; //seconds
+   public static final double STALL_CURRENT = 10.0;
+   public static final double STALL_MIN_VELOCITY = 100.0;
 
    private Shooter() {
         super(new HSFalcon(RobotMap.SHOOTER_MASTER_ID), new HSFalcon(RobotMap.SHOOTER_FOLLOWER_ID), 
             new DoubleSolenoid(RobotMap.FORWARD_SHOOTER_SOLENOID_PORT,RobotMap.REVERSE_SHOOTER_SOLENOID_PORT), 
             WHEEL_DIAMETER, TICKS_PER_REVOLUTION, GEAR_RATIO);
-        super.setupFlywheel(SENSOR_PHASE, MASTER_INVERT, FOLLOWER_INVERT, VELOCITY_SLOT, velocityPIDConstants, VOLTAGE_COMP ,   PEAK_CURRENT, SUSTAINED_CURRENT, PEAK_TIME);
+        super.setupFlywheel(SENSOR_PHASE, MASTER_INVERT, FOLLOWER_INVERT, VELOCITY_SLOT, velocityPIDConstants, VOLTAGE_COMP, CURRENT_LIMIT);
     }
 
    public static Shooter getInstance() {
@@ -41,5 +48,34 @@ public class Shooter extends HSFlywheel<HSMotorController> {
          shooter = new Shooter();
       }
       return shooter;
+   }
+
+   @Override
+   public void toggleSolenoid()
+   {
+      super.toggleSolenoid();
+   }
+
+   @Override
+   public void spinShooterPercentOutput(double percentOutput)
+   {
+      super.spinShooterPercentOutput(percentOutput);
+   }
+
+   @Override
+   public void spinShooterVelocity(double velocity)
+   {
+      super.spinShooterVelocity(velocity);
+   }
+
+   public boolean checkStalling()
+   {
+      return super.checkStalling(STALL_CURRENT, STALL_MIN_VELOCITY);
+   }
+
+   @Override
+   public void periodic() 
+   {
+
    }
 }
